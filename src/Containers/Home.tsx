6ea5025d944c46aa278bdb984fad27e881f396e6
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-03-09 13:02:32 +0600

    create home & navbar components

---
+++ src/Containers/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from "../Components/Navbar/Navbar.tsx";
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <Navbar/>
+            <div>
+                Home component
+            </div>
+        </div>
+    );
+};
+
+export default Home;
